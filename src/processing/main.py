7965fa8c0f142a5518e3ef44c179b6bdccc9433d
Juanan4290 <juanan4290@gmail.com>
2018-05-20 20:39:30 +0200

    process loan status and numerical variables on src

--- src/processing/main.py
+++ src/processing/main.py
@@ -9,24 +9,24 @@ Created on Sun May 20 17:17:04 2018
 ### 01 - Libraries ############################################################
 
 import pandas as pd
-
 import matplotlib.pyplot as plt; plt.style.use("ggplot")
-
 from src.processing.process_loan_status import process_loan_status
-
-### 02 - Read the data ########################################################
-
-loans = pd.read_csv("./data/clean/loans.csv", sep = "^")
+from src.processing.process_numerical_variables import process_numerical_variables
 
 
-### 03 - Process Target #######################################################
+if __name__ == '__main__':
 
-loans['loan_status'] = loans['loan_status'].map(process_loan_status)
-
-loans = loans[loans['loan_status']<2]
-
-
-### 04 - Writing our sample data 
-
-#loans.to_csv('../../data/loans_pro.csv', sep = "^", index=False)
+    ### 02 - Read the data ####################################################
+    
+    loans = pd.read_csv("../../data/clean/loans.csv", sep = "^").sample(200000, random_state=4290)
+    
+    
+    ### 03 - Processing variables #############################################
+    
+    loans = process_loan_status(loans)
+    loans = process_numerical_variables(loans)
+    
+    ### 04 - Writing our sample data 
+    
+    #loans.to_csv('../../data/loans_pro.csv', sep = "^", index=False)
 
