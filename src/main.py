955b7a4482d93da93d0330a8056dc3c96d706509
Juanan4290 <juanan4290@gmail.com>
2018-06-12 17:51:30 +0200

    logistic regression complete implementation

--- src/main.py
+++ src/main.py
@@ -9,30 +9,29 @@ Created on Sun May 20 17:17:04 2018
 ### 1. Libraries ###
 
 import pandas as pd
-import numpy as np
 
+from src.utils import categorical_to_numeric, reject_outliers
 from src.models.logistic_regression import logistic_regression
-from src.feature_engineering.grade import feature_grade
 
 
 if __name__ == '__main__':
 
     ### 2. read data ###
-    loans = pd.read_csv("/media/juanan/DATA/loan_data_analysis/data/loans_processed.csv", sep = "^").sample(200000, random_state=4290)
+    loans = pd.read_csv("/media/juanan/DATA/loan_data_analysis/data/loans_processed.csv", sep = "^")\
+                        .sample(20000)
     
-    ### 3. feature engineering
-    no_important_features = ["bc_open_to_buy", "delinq_2yrs", "fico_range_low", "funded_amnt_inv", "funded_amnt",
-                             "num_accts_ever_120_pd", "num_bc_tl", "num_il_tl", "num_op_rev_tl", "num_rev_accts",
-                             "num_sats", "open_acc", "pub_rec", "pub_rec_bankruptcies", "revol_bal",
-                             "tax_liens", "tot_coll_amt", "total_bal_ex_mort", "last_fico_range_high"]
+    ### 3. pre-processing
+    # numeric variables
+    numeric_variables = loans._get_numeric_data().columns
+    loans = reject_outliers(loans, numeric_variables, z_score = 3.5)
     
-    loans = loans.drop(no_important_features, axis = 1)
+    # categorical variables
+    categorical_variables = loans.select_dtypes(include="object").columns
     
-    loans['fico_range_square'] = np.square(loans['fico_range_high'])
-    loans['fico_range_cos'] = np.cos(loans['fico_range_high'])
-    loans['fico_range_sin'] = np.sin(loans['fico_range_high'])
+    for variable in categorical_variables:
+        loans[variable] = categorical_to_numeric(loans, variable, "loan_status")
     
     ### 4. logistic regression
-    log_reg = logistic_regression(loans, normalization = "robust", C = 0.5)
-    
+    print("LOGISTIC REGRESSION MODEL ----------------------------")
+    log_reg, logit_metrics = logistic_regression(loans, normalization = "standard")
     
--- src/models/logistic_regression.py
+++ src/models/logistic_regression.py
@@ -9,11 +9,10 @@ import pandas as pd
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import roc_auc_score, confusion_matrix, precision_recall_fscore_support, accuracy_score
 
-from src.utils import normalize_variables
+from src.utils import normalize_variables, model_evaluation
 
-def logistic_regression(data, C = 1, normalization = "robust"):
+def logistic_regression(data, normalization = "robust"):
     """
     Parameters
     ---------
@@ -29,21 +28,17 @@ def logistic_regression(data, C = 1, normalization = "robust"):
     print("Preprocessing...")
     # normalization
     data = normalize_variables(data, normalization)
-    
-    # one-hot-encoding
-    categorical_variables = data.select_dtypes(include="object").columns
-    data = pd.get_dummies(data, categorical_variables)
-    
+        
     # train/test split
     X = data.loc[:, data.columns!='loan_status']
     y = data['loan_status']
     
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=4290)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     
     ### model
     print("Fitting the model...")
     # logistic regression
-    log_reg = LogisticRegression(penalty='l2', C = C)
+    log_reg = LogisticRegression(penalty='l2', C = 10)
     log_reg.fit(X_train, y_train)
     
     ### evaluation
@@ -51,25 +46,12 @@ def logistic_regression(data, C = 1, normalization = "robust"):
     # scores
     y_scores_train = pd.DataFrame(log_reg.predict_proba(X_train)).loc[:,1]
     y_scores_test = pd.DataFrame(log_reg.predict_proba(X_test)).loc[:,1]
-
-    # auc
-    auc_train = roc_auc_score(y_train, y_scores_train)
-    auc_test = roc_auc_score(y_test, y_scores_test)
     
-    print("AUC:")
-    print("AUC in the train set: {}".format(auc_train))
-    print("AUC in the test set: {}".format(auc_test))
-    print("------------------------")
+    # writing scores
+    y_scores_train.to_csv("../output/y_scores_train_logit.csv", sep = "^")
+    y_scores_test.to_csv("../output/y_scores_test_logit.csv", sep = "^")
     
-    # confusion matrix and accuracy
-    y_test_pred = log_reg.predict(X_test)
+    metrics = model_evaluation(y_train, y_test, y_scores_train, y_scores_test)
     
-    print("Confusion Matrix:")
-    print(confusion_matrix(y_test, y_test_pred))
-    print("Accuracy: {}".format(accuracy_score(y_test, y_test_pred)))
-    print("Recall: {}".format(precision_recall_fscore_support(y_test, y_test_pred)[0]))
-    print("Precision: {}".format(precision_recall_fscore_support(y_test, y_test_pred)[1]))
-    print("------------------------")
-    
-    return log_reg
+    return log_reg, metrics
     
