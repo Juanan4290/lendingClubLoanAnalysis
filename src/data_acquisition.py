862565f9f8c673d07771336767dff9ad2b75da36
Juanan4290 <juanan4290@gmail.com>
2018-06-18 08:59:23 +0200

    selecting variables at data_acquisition.py

--- src/data_acquisition.py
+++ src/data_acquisition.py
@@ -23,10 +23,24 @@ for file in list_dir:
     
 loans = pd.concat(list_)
 
+## selecting variables
+print("Selecting variables...")
+
+columns_of_interest = ['num_bc_sats', 'num_rev_tl_bal_gt_0', 'grade', 'avg_cur_bal', 'pub_rec_bankruptcies', 
+                       'num_rev_accts', 'tax_liens', 'funded_amnt_inv', 'delinq_2yrs', 'total_bal_ex_mort',
+                       'pct_tl_nvr_dlq', 'disbursement_method', 'fico_range_low', 'verification_status', 'delinq_amnt',
+                       'purpose', 'emp_title', 'zip_code', 'loan_amnt', 'installment',
+                       'fico_range_high', 'annual_inc', 'term', 'int_rate', 'emp_length',
+                       'revol_bal', 'application_type', 'num_bc_tl', 'num_sats', 'tot_hi_cred_lim', 
+                       'tot_coll_amt', 'initial_list_status', 'bc_open_to_buy', 'total_bc_limit', 
+                       'open_acc', 'revol_util', 'pub_rec', 'funded_amnt', 'num_il_tl', 
+                       'addr_state', 'num_accts_ever_120_pd', 'total_il_high_credit_limit', 'bc_util', 'percent_bc_gt_75', 
+                       'sub_grade', 'mort_acc', 'num_op_rev_tl', 'dti', 'home_ownership', 'loan_status']
+
+loans = loans[sorted(columns_of_interest)]
+
 # Remove duplicates:
 loans = loans.drop_duplicates()
 
 ### 3. write data ###
-
-loans.to_csv("/media/juanan/DATA/loan_data_analysis/data/raw/accepted_2007_to_2017Q3.csv.gz", compression="gzip",
-             sep = "^", index=False)
+loans.to_csv("/media/juanan/DATA/loan_data_analysis/data/clean/loans.csv.gz", compression="gzip")
