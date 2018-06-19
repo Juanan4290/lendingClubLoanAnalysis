c8a925debfe0c3bb3d59311d4fd77d87ee8b4a13
Juanan4290 <juanan4290@gmail.com>
2018-06-19 17:03:23 +0200

    preprocessing implementation complete in main program

--- src/cleaning_data.py
+++ src/cleaning_data.py
@@ -12,11 +12,13 @@ warnings.filterwarnings('ignore')
 import numpy as np
 import pandas as pd
 
+from utils import process_emp_title
+
 
 ### 2. read data ###
 print("Reading Data...")
 
-loans = pd.read_csv("/media/juanan/DATA/loan_data_analysis/data/raw/accepted_2007_to_2017Q3.csv.gz")
+loans = pd.read_csv("/media/juanan/DATA/loan_data_analysis/data/clean/loans.csv.gz")
 
 
 ### 3. cleaning data ###
@@ -40,28 +42,12 @@ loans = loans.loc[:,na_percentage < 0.25]
 number_of_unique_values_per_column = loans.apply(lambda i: i.nunique(), axis = 0)
 loans = loans.loc[:,number_of_unique_values_per_column != 1]
 
-## selecting variables
-print("Selecting variables...")
-
-columns_of_interest = ['num_bc_sats', 'num_rev_tl_bal_gt_0', 'grade', 'avg_cur_bal', 'pub_rec_bankruptcies', 
-                       'num_rev_accts', 'tax_liens', 'funded_amnt_inv', 'delinq_2yrs', 'total_bal_ex_mort',
-                       'pct_tl_nvr_dlq', 'disbursement_method', 'fico_range_low', 'verification_status', 'delinq_amnt',
-                       'purpose', 'emp_title', 'zip_code', 'loan_amnt', 'installment',
-                       'fico_range_high', 'annual_inc', 'term', 'int_rate', 'emp_length',
-                       'revol_bal', 'application_type', 'num_bc_tl', 'num_sats', 'tot_hi_cred_lim', 
-                       'tot_coll_amt', 'initial_list_status', 'bc_open_to_buy', 'total_bc_limit', 
-                       'open_acc', 'revol_util', 'pub_rec', 'funded_amnt', 'num_il_tl', 
-                       'addr_state', 'num_accts_ever_120_pd', 'total_il_high_credit_limit', 'bc_util', 'percent_bc_gt_75', 
-                       'sub_grade', 'mort_acc', 'num_op_rev_tl', 'dti', 'home_ownership', 'loan_status']
-
-loans = loans[sorted(columns_of_interest)]
-
 
 ### 4. processing numeric variables ###
 print("Processing numeric variables...")
 
 numeric_variables = loans._get_numeric_data().columns
-## fill NA's to median
+## fill NA's to the median
 loans[numeric_variables] = loans[numeric_variables].apply(lambda i: i.fillna(i.median()), axis = 1)
 
 
@@ -71,12 +57,11 @@ print("Processing categorical variables...")
 categorical_variables = loans.select_dtypes(include="object").columns
 ## fill NA's to last valid observation
 loans[categorical_variables] = loans[categorical_variables].fillna(method = "ffill")
-
+## process emp_title variable
+loans["emp_title"] = process_emp_title(loans, n = 1)
 
 ### 6. writing clean data ###
 print("Writing data to disk...")
-# local directory: "/media/juanan/DATA/loan_data_analysis/data/loans_processed.csv"
-# relative directory: "../data/loans_processed.csv"
 print("Final dataset with {} rows and {} columns".format(loans.shape[0], loans.shape[1]))
 loans.to_csv("/media/juanan/DATA/loan_data_analysis/data/loans_processed.csv", sep = "^", index = False)
 print("Done!")
